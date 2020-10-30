d31a24c289199ff9ad293fb19eaad612da21399b
kobelatonio <kobebryan5598@gmail.com>
2020-10-30 11:46:20 +0800

    Migration for positions

---
+++ database/migrations/2020_10_30_033903_create_positions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePositionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('positions', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->decimal('monthly_salary', 20, 2);
+            $table->time('shift_start');
+            $table->time('shift_end');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('positions');
+    }
+}
