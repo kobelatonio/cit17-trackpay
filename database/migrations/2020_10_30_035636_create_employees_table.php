3afa8e7d1fc3d6d8b20f7975bb885f452d5d7193
kobelatonio <kobebryan5598@gmail.com>
2020-10-30 12:01:32 +0800

    Migration for leave categories, deductibles, and employees

---
+++ database/migrations/2020_10_30_035636_create_employees_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('contact_number');
+            $table->string('email_address');
+            $table->date('birthdate');
+            $table->string('gender');
+            $table->string('username');
+            $table->string('password');
+            $table->foreign('position_id')->references('id')->on('positions');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
